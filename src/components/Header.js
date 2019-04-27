92f5cdeba3d2fdbbaa7057584abd51b992b49ea8
davidholyko <david.holy.ko@gmail.com>
2019-04-27 00:15:08 -0400

    seperate header items

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,30 +3,40 @@ import Scrollspy from 'react-scrollspy'
 import $ from 'jquery'
 
 const Header = () => {
-  const sections = 'top about-me skills projects contact'.split(' ')
-  const sectionsText = ['Top', 'About Me', 'Skills', 'Projects', 'Contact']
+  const sections = [
+    { href: 'top',
+      text: 'David Ko' },
+    { href: 'about-me',
+      text: 'About Me' },
+    { href: 'skills',
+      text: 'Skills' },
+    { href: 'projects',
+      text: 'Projects' },
+    { href: 'contact',
+      text: 'Contact' }
+  ]
 
   const scrollTo = element => {
     if (element === '#top') {
       return $('html,body').animate({ scrollTop: $(`${element}`).offset().top }, 500)
     }
-
-    console.log(element)
-
     $('html,body').animate({ scrollTop: $(`${element}`).offset().top + 1 }, 500)
   }
   const handleClick = () => scrollTo(event.target.getAttribute('href'))
 
+  const generateHeaderText = (sections, className) => sections.map((section, index) => (
+    <a key={index}
+      href={`#${section.href}`}
+      className={'header-item px-2 ' + className}
+      onClick={handleClick}>
+      <h3 className="header-item-text" href={`#${section.href}`}>{section.text}</h3>
+    </a>
+  ))
+
   return (
-    <Scrollspy items={sections} currentClassName="is-current" className='header'>
-      {sections.map((section, index) => (
-        <a key={index}
-          href={`#${section}`}
-          className='header-item'
-          onClick={handleClick}>
-          <h3 className="header-item-text" href={`#${section}`}>{sectionsText[index]}</h3>
-        </a>
-      ))}
+    <Scrollspy items={sections.map(section => section.href)} currentClassName="is-current" className='header'>
+      {generateHeaderText([sections[0]], 'mr-auto')}
+      {generateHeaderText(sections.slice(1))}
     </Scrollspy>
   )
 }
