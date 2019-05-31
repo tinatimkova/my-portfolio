2a591f773fa509264113fa292773ac7f08a56b5b
dko-challenges <dko.challenges@gmail.com>
2019-05-31 17:54:18 -0400

    temp: remove picture; change header bg color

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import src from '../../images/selfie.webp'
+// import src from '../../images/selfie.webp'
 
 class AboutMe extends Component {
   render () {
@@ -8,18 +8,31 @@ class AboutMe extends Component {
 
     My experience playing this game carries onto being web developer by allowing me to use my abilites to pickup new information quickly and apply them, by helping me practice communicating effectively at a macro and micro level, and by giving me the opportunity to share information with others and improve from feedback.`
 
+    // return (
+    //   <div id="about-me" className="section">
+    //     <h1 className="section-title">My Story</h1>
+    //     <div className="about-me-sub-section">
+    //       <div className="col-12 col-sm-8">
+    //         {text.split('\n').map((paragraph, index) => (
+    //           <p key={index} className="mb-4">{paragraph}</p>
+    //         )
+    //         )}
+    //       </div>
+    //       <div className="col-12 col-sm-4">
+    //         <img src={src} alt="" className="selfie"/>
+    //       </div>
+    //     </div>
+    //   </div>
+    // )
     return (
       <div id="about-me" className="section">
         <h1 className="section-title">My Story</h1>
         <div className="about-me-sub-section">
-          <div className="col-12 col-sm-8">
+          <div className="col-12 col">
             {text.split('\n').map((paragraph, index) => (
               <p key={index} className="mb-4">{paragraph}</p>
             )
             )}
-          </div>
-          <div className="col-12 col-sm-4">
-            <img src={src} alt="" className="selfie"/>
           </div>
         </div>
       </div>
