3dee495737b84fc34ea33ecb14d07c3a71e93383
timosha <tinaatimkovaa@gmail.com>
2020-03-21 18:28:10 -0400

    Completed about me section
    wrote new about me introduction
    changed the section's layout

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -3,22 +3,18 @@ import src from '../../images/ProfilePic.JPG';
 
 class AboutMe extends Component {
   render () {
-    const text = 'I’m a full stack developer with experience primarily in JavaScript although Ruby and Python are also in my field of interest. I want to be a junior developer at a company where I can learn, grow, and continue to master my programming skills.';
+    const text = 'I\'m a full-stack developer with a passion for problem-solving and attention to detail. I strongly believe technologies can make the world a better place if we want them to.\n I have experience in customer service, teaching, and digital marketing. There\'re multiple skills I can transfer to web development from my previous experience including critical thinking, customer orientation, and adaptability.\n I\'m resourceful, persevering, always demonstrating enthusiasm and commitment to my work. In others, I value integrity, modesty, curiosity, and a sense of humor.\n My goal is to build a career in web development. My current fields of interest are React-related development, back-end development with Ruby on Rails and Express. My other interests include outdoor activities (hiking, biking, skiing) and social media.';
 
     return (
       <div id="about-me" className="section">
         <h1 className="section-title">About Me</h1>
         <div className="about-me-sub-section">
-          <div className="col-12 col-md-7">
-            {text.split('\n').map((paragraph, index) => (
-              <h5 key={index} className="mb-4">
-                {paragraph}
-              </h5>
-            ))}
-          </div>
-          <div className="col-12 col-md-5">
-            <img src={src} alt="" className="selfie" />
-          </div>
+          <img src={src} alt="" className="selfie" />
+          {text.split('\n').map((paragraph, index) => (
+            <p key={index} className="about-me-text mb-4">
+              {paragraph}
+            </p>
+          ))}
         </div>
       </div>
     );
